a24116319ecde3fff6d1e0554b69d02376b0b0bc
mattk <mkimucsb@gmail.com>
2023-11-30 11:17:36 -0800

    added delete functionality for items in cart

--- app/api/cart_routes.py
+++ app/api/cart_routes.py
@@ -5,4 +5,20 @@ from app.forms import get_unique_filename, upload_file_to_s3, error_messages,err
 
 cart_routes = Blueprint('carts', __name__)
 
-# @cart_routes.route('/<int:id>')
+@cart_routes.route('/<int:frogId>', methods=['DELETE'])
+@login_required
+def delete_frog_from_cart(frogId):
+    """
+    Deletes a frog from a user's cart
+    """
+    cart = Cart.query.filter(Cart.user_id == current_user.id).first()
+
+    
+    index_to_delete = next((index for index, item in enumerate(cart.items) if item.id == frogId), None)    
+    if index_to_delete is not None:
+        # Remove the item from the list and commit the changes to the database
+        del cart.items[index_to_delete]
+        db.session.commit()
+        return error_message("message", "Deleted"), 200
+    else:
+        return error_message("message", "Item not found in the cart"), 404
